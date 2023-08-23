865e07ba3bee8b1635a7ab365d53d1d9a8baa756
Alice Lin <alicexxlin@gmail.com>
2023-08-23 12:00:06 -0600

    implement function getComputerChoice

---
+++ functions.js
@@ -0,0 +1,7 @@
+const getComputerChoice = () => {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
+
+console.log(`Computer choice: ${getComputerChoice()}`);
