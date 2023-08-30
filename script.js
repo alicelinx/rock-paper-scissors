8cc76ee273d3fa0968f5bfe3046f6039b168c5ed
Alice Lin <alicexxlin@gmail.com>
2023-08-29 19:14:11 -0600

    add styles

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const choices = ['rock', 'paper', 'scissors'];
+const gestures = {
+  "rock": '✊',
+  "paper": '🖐️',
+  "scissors": '✌️'
+};
 const buttons = document.querySelectorAll('button.gesture-button');
 const scoreStatus = document.querySelector('.score');
 const resultDisplay = document.querySelector('.result');
 const restartButton = document.querySelector('button.restart-button');
+const gestureDisplay = document.querySelector('.image-display');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -17,6 +23,7 @@ const updateScore = () => {
 };
 
 const playRoundLogic = (playerSelection, computerSelection) => {
+  gestureDisplay.textContent = `${gestures[playerSelection]} vs ${gestures[computerSelection]}`;
   if (playerSelection === computerSelection) {
     return "It's a tie, try it again!";
   }
@@ -28,7 +35,6 @@ const playRoundLogic = (playerSelection, computerSelection) => {
   ) {
     return `You lose... ${playerSelection} loses to ${computerSelection} 🙁`;
   }
-  console.log([playerSelection, computerSelection]);
   return `You win! ${playerSelection} beats ${computerSelection} 🥳`;
 
 };
@@ -47,7 +53,6 @@ const playRound = (e) => {
     computerScore++;
 
   }
-  console.log(computerSelection);
   updateScore();
 
   if (playerScore >= 5) {
@@ -62,6 +67,7 @@ const restartGame = () => {
   computerScore = 0;
   updateScore();
   resultDisplay.textContent = null;
+  gestureDisplay.textContent = null;
 };
 
 restartButton.addEventListener('click', restartGame);
